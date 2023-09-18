4c9c3b1c81509a25d13cdfd6dc2802db7883f884
vivek.v <paivivek002@gmail.com>
2023-09-18 19:58:57 +0530

    certification added

--- script.js
+++ script.js
@@ -76,3 +76,22 @@ function open_link(link_name) {
 
   window.open(URL, "_blank");
 }
+
+// =================================================================================================
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
