87fa80f6c60a5ed1eb0eaedc778362b32d437e74
vivek-metricdust <vivek.v@metricdust.com>
2025-10-17 23:09:00 +0530

    web dev section completed

--- src/components/webApps.tsx
+++ src/components/webApps.tsx
@@ -12,9 +12,11 @@ const WebApps = () => {
       name: "ShopProp",
       logo: "/images/projects/shopprop.png",
       url: "https://www.shopprop.com",
+      description:
+        "ShopProp is a real estate e-commerce platform that allows users to buy and sell properties online.",
+      moredetails: [],
     },
     // {
-
     //   name: "Metric Realties",
     //   logo: "/images/projects/MR_Logo_new.png",
     //   url: "https://www.metricrealties.com",
@@ -108,7 +110,7 @@ const WebApps = () => {
     },
   ];
 
-  const [selectedWebApp, setSelectedWebApp]: any = useState("");
+  const [selectedWebApp, setSelectedWebApp] = useState<any>("");
 
   useEffect(() => {
     const handleTyping = () => {
@@ -216,7 +218,57 @@ const WebApps = () => {
             </div>
           ) : (
             <div className="chrome-content">
-              
+              <div className="chrome-search">
+                <div className="search-bar">
+                  <div className="search-icon">
+                    <svg
+                      viewBox="0 0 24 24"
+                      width="20"
+                      height="20"
+                      fill="#9AA0A6"
+                    >
+                      <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                    </svg>
+                  </div>
+                  <div
+                    className="search-input text-start "
+                    style={{ fontSize: "16px" }}
+                  >
+                    {selectedWebApp.name}
+                  </div>
+                  <div className="mic-icon">
+                    <svg
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      fill="#4285F4"
+                    >
+                      <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z" />
+                      <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z" />
+                    </svg>
+                  </div>
+                </div>
+
+                <div className="glass ">
+                  <img
+                    className="w-25"
+                    src={selectedWebApp.logo}
+                    alt={selectedWebApp.name}
+                  />
+                  <a
+                    href={selectedWebApp.link}
+                    target="_blank"
+                    style={{
+                      color: "var(--accent)",
+                      fontSize: "16px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {selectedWebApp.url}
+                  </a>
+                  <div>{selectedWebApp.description}</div>
+                </div>
+              </div>
             </div>
           )}
         </div>
