4ded12fbb6bc8b6c1100f7c59ebc51044f2c867e
vivek-metricdust <vivek.v@metricdust.com>
2025-10-17 20:05:29 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Landing from "./components/landing";
-
+import WebApps from "./components/webApps";
 function App() {
   return (
     <div className="">
       <div className="blob b1"></div>
       <div className="blob b2"></div>
       <Landing />
-      test app
+      <WebApps />
     </div>
   );
 }
