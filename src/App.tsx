91138441510ff4af53d72263c0f306d5fd7d54bc
vivek-metricdust <vivek.v@metricdust.com>
2025-10-17 23:59:24 +0530

    mobile added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Landing from "./components/landing";
 import WebApps from "./components/webApps";
+import PhoneApps from "./components/phoneApps";
 function App() {
   return (
     <div className="">
@@ -9,6 +10,7 @@ function App() {
       <div className="blob b2"></div>
       <Landing />
       <WebApps />
+      <PhoneApps />
       <div className="mt-4 mb-4"></div>
     </div>
   );
