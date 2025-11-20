a7d68be834f79aa1d6bd4405dedb606f628594b9
vivekvpai <paivivek002@gmail.com>
2025-11-21 00:00:00 +0530

    feat: Add a new component to display blog posts with a terminal-style card design.

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -27,23 +27,44 @@ const Blogs = () => {
 
   return (
     <div className="container mt-48">
-      <div className="section-title pt-48">My Blogs</div>
+      <div className="section-title pt-48">
+        <span className="comment-style">// My Technical Writings</span>
+      </div>
 
-      <div className="blogs ">
+      <div className="blogs">
         {blogs.map((blog, index) => (
           <a
             href={blog.link}
             target="_blank"
             rel="noopener noreferrer"
             key={index}
-            className="my-2 glass blog-card"
+            className="dev-blog-card"
           >
-            <img
-              className="blog-logo"
-              src={process.env.PUBLIC_URL + blog.img}
-              alt={blog.name}
-            />
-            <div className="ms-2 blog-name">{blog.name}</div>
+            <div className="window-header">
+              <div className="window-dots">
+                <span className="dot red"></span>
+                <span className="dot yellow"></span>
+                <span className="dot green"></span>
+              </div>
+              <div className="window-title">post_{index + 1}.md</div>
+            </div>
+            <div className="window-content">
+              <div className="img-container">
+                <img
+                  className="blog-image"
+                  src={process.env.PUBLIC_URL + blog.img}
+                  alt={blog.name}
+                />
+                <div className="overlay">
+                  <span className="read-cmd">{">"} read</span>
+                </div>
+              </div>
+              <div className="blog-info">
+                <span className="const-keyword">const</span>{" "}
+                <span className="var-name">title</span> ={" "}
+                <span className="string-value">"{blog.name}"</span>;
+              </div>
+            </div>
           </a>
         ))}
       </div>
